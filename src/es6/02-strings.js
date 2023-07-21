76729c874552874b4b43bb4fc92683632cb4df0d
Alexei Mamani <alexeimc763@gmail.com>
2023-07-21 10:40:31 -0500

    Template literals

---
+++ src/es6/02-strings.js
@@ -0,0 +1,20 @@
+// Template literals
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+
+let firstName = "Alexei";
+let lastName = "Mamani";
+
+console.log(firstName + " " + lastName); // old way
+console.log(`${firstName} ${lastName}`); // new way
+
+// Multiple lines
+console.log(
+  "This is an string with multiple line \n" + "The second part of the line"
+);
+
+const paragraph = `
+This is an string with multiple lines
+The second part of the line
+`;
+
+console.log(paragraph);
