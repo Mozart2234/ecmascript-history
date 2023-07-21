95e9ac6605fc7bd4b2bdbdc1396f2d6ccb3ee7b4
Alexei Mamani <alexeimc763@gmail.com>
2023-07-21 10:19:34 -0500

    Examples of new definition variables with ES6

---
+++ src/es6/00-variables.js
@@ -0,0 +1,35 @@
+// In previous JS versions the way to define the variables was defined by the
+// keyword var
+
+var oldWay = "This is previous ES6";
+console.log(oldWay);
+oldWay = "We can redefine it";
+console.log(oldWay);
+
+// The new ways to define variables after ES6 are let and const
+
+let mood = "Happy";
+console.log(mood);
+mood = "Sad";
+console.log(mood);
+
+const gravity = 9.81;
+gravity = 10.2; // this gonna raise an exception TypeError: Assignment to constant variable.
+console.log(gravity);
+
+// the scope of those are block, so it means the scope of this works in the block
+// if we would like to access to them outside the block we're gonna have an exception.
+
+const f1Names = () => {
+  if (true) {
+    var ferrariPilot = "Charles Leclerc";
+    let mercedesPilot = "Lewis Hamilton";
+    const redBullPilot = "Max Verstappen";
+  }
+
+  console.log(ferrariPilot);
+  console.log(mercedesPilot); // ReferenceError: mercedesPilot is not defined
+  console.log(redBullPilot);
+};
+
+f1Names();
