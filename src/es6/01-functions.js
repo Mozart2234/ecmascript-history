36735202b03d154a2c6a08fec4783b34dd6809e2
Alexei Mamani <alexeimc763@gmail.com>
2023-07-21 10:30:01 -0500

    Arrow functions

---
+++ src/es6/01-functions.js
@@ -0,0 +1,14 @@
+//old way
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+//
+
+const sum = (num1, num2) => {
+  return num1 + num2;
+};
+
+// We could use explicit and sometimes can simplify the code
+const sum = (num1, num2) => num1 + num2;
