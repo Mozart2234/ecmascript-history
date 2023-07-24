1e585c668a06e7f32be0d2839e3b0af92fdff207
Alexei Mamani <alexeimc763@gmail.com>
2023-07-24 10:36:45 -0500

    Modules ES6

---
+++ src/es6/09-modules-export.js
@@ -0,0 +1,8 @@
+// 09-modules
+// Ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules
+
+const hello = () => {
+  console.log("Hellos from exported");
+};
+
+export default hello;
---
+++ src/es6/09-modules-import.js
@@ -0,0 +1,6 @@
+// 09-modules
+// Ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules
+
+import hello from "./09-modules-export.js";
+
+hello();
